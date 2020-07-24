6ff6fdc33c4c071b103ebe5d1dfbf328f02a82be
kouichiUme <kouichiume@gmail.com>
2020-07-25 02:04:43 +0900

    session request test

---
+++ requestcrsf.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+import requests
+import json
+
+
+url = "http://127.0.0.1:8000/ArmRestApi/loadProgramFile"
+sess = requests.session()
+
+print(sess.get(url))
+
+csrftoken = sess.cookies['csrftoken']
+
+# ヘッダ
+headers = {'Content-type': 'application/json',  "X-CSRFToken": csrftoken}
+
+# 送信データ
+prm = {"data":"hogegeho","param1": "ぎ　 ", "param2": "パラメータ２"}
+
+# JSON変換
+params = json.dumps(prm)
+
+print(params)
+# POST送信
+res = sess.post(url, data=params, headers=headers)
+
+# 戻り値を表示
+print(json.loads(res.text))
