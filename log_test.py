02645b358bf2c252446938b243f7fff9a9de2aaa
kouichiUme <kouichiume@gmail.com>
2020-10-24 23:16:00 +0900

    git ignore2

---
+++ log_test.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+
+import requests
+import json
+
+
+
+url = "http://127.0.0.1:8000/ArmRestApi/loadProgram"
+sess = requests.session()
+res = sess.get(url)
+print(json.loads(res.text))
+
+
+# insert  teachingNewPosition
+
+url = "http://127.0.0.1:8000/ArmRestApi/log"
+csrftoken = sess.cookies['csrftoken']
+# 
+headers = {'Content-type': 'application/json',  "X-CSRFToken": csrftoken}
+# 
+prm = {"cmd":"foobar"}
+# 
+params = json.dumps(prm)
+print(params)
+# post test data
+res = sess.get(url, data=params, headers=headers)
+
+
+print(json.loads(res.text))
+
+
+
